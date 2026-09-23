b5373c4f4466aed95a9e5a0caa755218f56b682c
leon <wakanyileonard@gmail.com>
2026-09-23 19:50:07 +0300

    add:allow frontend to talk to the backend through path if on different origins

--- server.js
+++ server.js
@@ -5,13 +5,14 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname)); // serves index.html, style.css, script.js
+app.use(express.static(path.join(__dirname, 'public'))); // serves index.html, style.css, script.js
 
 /* ==========================================================================
    In-memory "garage operations system" catalog
