cb7ad77c81b8f7ac1c1d62f0bcef775a6feaf3f4
leon <wakanyileonard@gmail.com>
2026-09-24 21:57:17 +0300

    add:cors for frontend communication in diff ports

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,9 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
+const express = require('express');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
